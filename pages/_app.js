13785651c0b609fd84dbea53c6a503df8d765353
TIMMYLOPK <timmy.12345wer11@gmail.com>
2023-02-16 19:54:19 +0800

    feat: better meta data

--- pages/_app.js
+++ pages/_app.js
@@ -6,20 +6,23 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="ONCE: A profile website for Timmy" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="ONCE" />
-        <meta
-          property="og:description"
-          content="ONCE: A profile website for Timmy"
-        />
-        <meta property="og:url" content="https://lionceu.live" />
-        <meta property="og:image" content="/card.png" />
-        <meta name="author" content="Timmy" />
-        <link rel="apple-touch-icon" href="/favicon.ico" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <title>once.</title>
+        <meta name="description" content="ONCE: A profile website for Timmy" />
+        <meta property="og:url" content="https://www.lionceu.live/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="once" />
+        <meta property="og:description" content="ONCE: A profile website for Timmy" />
+        <meta property="og:image" content="https://lionceu.live/card.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="lionceu.live" />
+        <meta property="twitter:url" content="https://www.lionceu.live/" />
+        <meta name="twitter:title" content="once" />
+        <meta name="twitter:description" content="ONCE: A profile website for Timmy" />
+        <meta name="twitter:image" content="https://lionceu.live/card.png" />
+
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
       </Head>
       <ThemeProvider attribute="class" defaultTheme="system">
         <Component {...pageProps} />
