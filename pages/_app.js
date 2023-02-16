36064ffe65f010bb4dfe7ffa462b4d787fb07a5e
TIMMYLOPK <timmy.12345wer11@gmail.com>
2023-02-16 19:43:26 +0800

    feat: imporve meta data

--- pages/_app.js
+++ pages/_app.js
@@ -7,13 +7,18 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="HomePage@once" />
+        <meta name="description" content="ONCE: A profile website for Timmy" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="ONCE" />
+        <meta
+          property="og:description"
+          content="ONCE: A profile website for Timmy"
+        />
+        <meta property="og:url" content="https://lionceu.live" />
+        <meta property="og:image" content="/card.png" />
         <meta name="author" content="Timmy" />
         <link rel="apple-touch-icon" href="/favicon.ico" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta property="og:site_name" content="HomePage@once" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="/card.png" />
         <title>once.</title>
       </Head>
       <ThemeProvider attribute="class" defaultTheme="system">
