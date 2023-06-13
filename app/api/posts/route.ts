93b6b8ce989d3e227aba162b5bd664926d575a61
TIMMYLOPK <timmy.12345wer11@gmail.com>
2023-06-13 19:40:26 +0800

    fix: data not updated

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,10 +1,13 @@
 import { NextResponse } from "next/server";
+import { revalidatePath } from 'next/cache'
 
 export async function GET() {
   const req = await fetch(process.env.API_URL + "/posts").catch((e: any) => {
     return NextResponse.json({ error: e.message });
   });
   const posts = await req.json();
+
+  revalidatePath("/posts");
 
   return NextResponse.json({ posts });
 }
