14782301a1eba8fdc282a4c7ab7095a218191898
TIMMYLOPK <timmy.12345wer11@gmail.com>
2023-06-13 19:49:13 +0800

    refactor: log the time of res

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -2,12 +2,12 @@ import { NextResponse } from "next/server";
 import { revalidatePath } from 'next/cache'
 
 export async function GET() {
-  const req = await fetch(process.env.API_URL + "/posts").catch((e: any) => {
+  const req = await fetch(process.env.API_URL + "/posts", { next: { revalidate: 5 } }).catch((e) => {
     return NextResponse.json({ error: e.message });
   });
   const posts = await req.json();
 
   revalidatePath("/posts");
 
-  return NextResponse.json({ posts });
+  return NextResponse.json({ posts, timestamp: Date.now() });
 }
