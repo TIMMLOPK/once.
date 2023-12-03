9ff752afebab103166fdce775918d9f9f4a25955
TIMMYLOPK <timmy.12345wer11@gmail.com>
2023-12-03 20:47:45 +0800

    chore: deps

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/shop",
-        destination: "https://www.musicamatics.com/",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
