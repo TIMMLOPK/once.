49168bf5d5eed5979eb1c3611532ce52f3abc637
TIMMYLOPK <timmy.12345wer11@gmail.com>
2023-12-19 22:09:40 +0800

    fix: build

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { LazyMotion, domAnimation } from "framer-motion";
+import { useEffect , useState } from "react";
 import { ThemeProvider } from "next-themes";
 import Snowfall from 'react-snowfall';
 
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const snowflake = document.createElement('img');
-  snowflake.src = '/snowflake.png';
+  const [snowflake, setSnowflake] = useState<HTMLImageElement>();
+
+  useEffect(() => {
+    const snowflake = document.createElement('img');
+    snowflake.src = '/snowflake.png';
+    setSnowflake(snowflake);
+  }, []);
 
   return (
     <ThemeProvider attribute="class" disableTransitionOnChange>
