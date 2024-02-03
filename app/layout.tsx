28e013d88ddebcb17e2af99b13949c0412e56d80
TIMMYLOPK <timmy.12345wer11@gmail.com>
2024-02-03 17:07:45 +0800

    chore: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import React from "react";
 import { Providers } from "./providers";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -31,6 +32,7 @@ export default function RootLayout({
         <Providers>
           {children}
           <SpeedInsights />
+          <Analytics />
         </Providers>
       </body>
     </html>
